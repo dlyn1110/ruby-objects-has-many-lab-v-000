4d86d61660cf8a9ab85454ce3da0f25915c58cec
Dani Lyn <dlyn1110@gmail.com>
2018-05-10 21:26:38 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,6 +15,11 @@ class Author
      post.author = self
    end
 
+   def add_post_by_title(title)
+       post = Post.new(title)
+       self.add_post(post)
+   end
+
    def add_post_by_name(name)
       post = post.new(name)
       @posts << post
@@ -22,10 +27,7 @@ class Author
    end
 
 
-   def add_post_by_title(title)
-       post = Post.new(title)
-       self.add_post(post)
-   end
+
 
    def self.post_count
      @@post_count
